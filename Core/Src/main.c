3c92e36138c8af887198b876c5bcfe849c9e3cfb
Gustavo Weber Denardin <gustavo.denardin@gmail.com>
2025-11-06 08:34:57 -0300

    minor fix

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,6 +23,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <string.h>
+#include "FreeRTOS_CLI.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
